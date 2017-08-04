518423cf01766712e51bc173b7ec708dd4920125
alcalapm <marc.alcala@roche.com>
2017-08-04 12:13:26 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# markdown-mume-renderer
+# Install dependencies
+
+```js
+npm install
+```
+
+# Build
+
+```js
+npm run build
+```
+
+# Start
+
+```js
+npm start <absolute origin file path> <absolute destination file path>
+```
---
+++ src/index.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+import * as mume from "@shd101wyy/mume";
+import * as fse from 'fs-extra';
+
+var program = require('commander');
+var filePath;
+var outputFilePath;
+
+/* Parse command parameters */
+program
+  .version('0.1.0')
+  .arguments('<input> <output>')
+  .action(function (input, output) {
+     filePath = input;
+     outputFilePath = output;
+  });
+program.parse(process.argv);
+
+
+/* Ensure parameters are correct */
+if (!filePath.includes(".md")) {
+  console.error("File to convert must be a .md file");
+  process.exit(1);
+}
+
+/* Convert md to html */
+async function main() {
+  console.info("File to convert: " + filePath);
+
+  await mume.init();
+
+  const engine = new mume.MarkdownEngine({
+    filePath: filePath,
+    config: {
+      previewTheme: "github-light.css",
+      codeBlockTheme: "default.css"
+    }
+  });
+
+  await engine.htmlExport({offline: false, runAllCodeChunks: true});
+
+  /* Move new file to destination path */
+  let origin = filePath.replace(".md", ".html");
+  console.info("Renamig file: " + origin);
+  console.info("   to: " + outputFilePath);
+  fse.moveSync(origin, outputFilePath, { overwrite: true });
+
+  return process.exit();
+}
+
+main(filePath, outputFilePath).catch(function (reason) {
+  console.error(reason);
+  process.exit(1);
+});
